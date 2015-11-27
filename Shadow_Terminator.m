fa8b2678b9a3d7d53779395e253e481a50ac0db0
jbauer7 <jwbauer3@wisc.edu>
2015-11-27 14:20:40 -0600

    first attempt for removal

--- Shadow_Terminator.m
+++ Shadow_Terminator.m
@@ -1,4 +1,7 @@
 img = imread('shadow_edit.jpg');
 img = im2double(img);
 img = shadow_detection(img);
+
+% Gaussian Filter to smooth transitions
+img = im2uint8(img);
 imwrite(img, 'shadow_out.jpg');
