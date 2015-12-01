96d2fc0db3103f01d46af0026f7f5b2609c58378
jbauer7 <jwbauer3@wisc.edu>
2015-12-01 12:51:46 -0600

    .m

--- Shadow_Terminator.m
+++ Shadow_Terminator.m
@@ -3,7 +3,11 @@ img = im2double(img);
 
     img = shadow_detection(img);
 
-img = imgaussfilt(img);
+%img = imgaussfilt(img);
 %Gaussian Filter to smooth transitions
+% fil = fspecial('gaussian', 9, .5);
+% img = conv2(img,fil);
+%IMG = imgaussfilt(img, 1100);
 img = im2uint8(img);
-imwrite(img, 'shadow_game_out.jpg');
+imshow(img);
+%imwrite(img, 'shadow_game_out.jpg');
